31340c880df3da31eeba77b4f0c240a12f43aed0
Freeftish <npkhang2005@gmail.com>
2025-06-07 19:02:32 +0700

    fixed entity implementation and resolved mysql dependency

--- backend/src/main/java/swp391/com/backend/pojo/Cycle.java
+++ backend/src/main/java/swp391/com/backend/pojo/Cycle.java
@@ -6,12 +6,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class Cycles {
+public class Cycle{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
--- backend/src/main/java/swp391/com/backend/pojo/Symptom.java
+++ backend/src/main/java/swp391/com/backend/pojo/Symptom.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
+
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class Symptoms {
+public class Symptom{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
