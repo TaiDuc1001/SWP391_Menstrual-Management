83308355d2fe6b515210e24203a079d623b43769
Freeftish <npkhang2005@gmail.com>
2025-06-08 17:36:14 +0700

    Created Appointment API

---
+++ backend/src/main/java/swp391/com/backend/service/appointments/AppointmentsService.java
@@ -0,0 +1,30 @@
+package swp391.com.backend.service.appointments;
+
+import org.springframework.stereotype.Service;
+import swp391.com.backend.pojo.appointments.Appointment;
+import swp391.com.backend.repository.appointments.AppointmentsRepository;
+
+import java.util.List;
+
+@Service
+public class AppointmentsService {
+    private final AppointmentsRepository appointmentsRepository;
+
+    public AppointmentsService(AppointmentsRepository appointmentsRepository) {
+        this.appointmentsRepository = appointmentsRepository;
+    }
+
+    public List<Appointment> getAllAppointments() {
+        return appointmentsRepository.findAll();
+    }
+
+    public void deleteAppointment(Integer id) {
+        Appointment appointment = appointmentsRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Appointment not found"));
+        appointmentsRepository.delete(appointment);
+    }
+
+    public Appointment createAppointment(Appointment appointment) {
+        return appointmentsRepository.save(appointment);
+    }
+}
