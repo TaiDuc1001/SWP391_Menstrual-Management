d84793c6c6e001210968a3ae44b5226f38c1dee4
Freeftish <npkhang2005@gmail.com>
2025-06-08 09:07:53 +0700

    Updated repository interfaces to reflect their function

--- backend/src/main/java/swp391/com/backend/repository/test/PackageRepository.java
+++ backend/src/main/java/swp391/com/backend/repository/test/PackageRepository.java
@@ -2,5 +2,5 @@ package swp391.com.backend.repository.test;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PackageTest extends JpaRepository<Package, Integer> {
+public interface PackageRepository extends JpaRepository<PackageRepository, Integer> {
 }
