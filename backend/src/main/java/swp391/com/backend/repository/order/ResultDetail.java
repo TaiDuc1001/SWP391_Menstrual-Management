063c073bf8623b79bc1426d2e215fa27b72460b5
Freeftish <npkhang2005@gmail.com>
2025-06-08 08:59:42 +0700

    Created additional core repositories for Order and Test packages

---
+++ backend/src/main/java/swp391/com/backend/repository/order/ResultDetail.java
@@ -0,0 +1,6 @@
+package swp391.com.backend.repository.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultDetail extends JpaRepository<ResultDetail, Integer> {
+}
---
+++ backend/src/main/java/swp391/com/backend/repository/test/PackageTest.java
@@ -0,0 +1,6 @@
+package swp391.com.backend.repository.test;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PackageTest extends JpaRepository<Package, Integer> {
+}
