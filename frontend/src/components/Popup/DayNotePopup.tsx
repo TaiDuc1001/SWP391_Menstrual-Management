5fd3876cc49d48706274fa06aadc88fe2eba5ff1
Tuan Anh <tuananhvn0203@gmail.com>
2025-06-16 00:55:46 +0700

    Edit file

--- frontend/src/components/Popup/DayNotePopup.tsx
+++ frontend/src/components/Popup/DayNotePopup.tsx
@@ -1,8 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
 import Popup from './ExitPopup';
-import React, { useState } from 'react';
-import Popup from './Popup';
 
 interface DayNotePopupProps {
   open: boolean;
