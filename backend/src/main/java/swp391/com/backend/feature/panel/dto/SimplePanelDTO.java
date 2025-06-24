9ea95c0192ef6f384f4b2b553cea73b804a2e5f0
LinhTran3568 <duclinht7@gmail.com>
2025-06-24 16:41:25 +0700

    Show test packages

--- backend/src/main/java/swp391/com/backend/feature/panel/dto/SimplePanelDTO.java
+++ backend/src/main/java/swp391/com/backend/feature/panel/dto/SimplePanelDTO.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import swp391.com.backend.feature.panel.data.PanelTag;
 import swp391.com.backend.feature.panel.data.PanelType;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,7 +16,10 @@ public class SimplePanelDTO {
     private Long id;
     private String panelName;
     private String description;
+    private BigDecimal price;
     private Integer responseTime;
     private PanelTag panelTag;
     private PanelType panelType;
+    private List<String> testTypesNames;
+    private List<String> testTypesDescriptions;
 }
