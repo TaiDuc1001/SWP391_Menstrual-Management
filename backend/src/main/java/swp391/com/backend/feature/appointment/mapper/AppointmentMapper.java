702f18dda593c0dccc936e10fbd8cfd1a22c57b5
KhangNPSE194017 <ledannhi78@gmail.com>
2025-07-01 17:39:24 +0700

    Changed appointments api to comply with HATEOAS infrastructure

--- backend/src/main/java/swp391/com/backend/feature/appointment/mapper/AppointmentMapper.java
+++ backend/src/main/java/swp391/com/backend/feature/appointment/mapper/AppointmentMapper.java
@@ -3,20 +3,14 @@ package swp391.com.backend.feature.appointment.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import swp391.com.backend.feature.appointment.dto.AppointmentDTO;
-import swp391.com.backend.feature.appointment.dto.SimpleAppointmentDTO;
 import swp391.com.backend.feature.appointment.data.Appointment;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface AppointmentMapper {
-    @Mapping(source = "doctor.name", target = "doctorName")
-    @Mapping(source = "customer.name", target = "customerName")
-    @Mapping(source = "customer.phoneNumber", target = "phoneNumber")
-    @Mapping(source = "slot.timeRange", target = "timeRange")
-    SimpleAppointmentDTO toSimpleDTO(Appointment appointment);
-
     Appointment toEntity(AppointmentDTO dto);
+
     @Mapping(source = "customer.id", target = "customerId")
     @Mapping(source = "customer.name", target = "customerName")
     @Mapping(source = "customer.phoneNumber", target = "phoneNumber")
