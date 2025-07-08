723528872f89413e56dea37e6a5377aa442d4a28
LinhTran3568 <duclinht7@gmail.com>
2025-07-08 18:31:51 +0700

    update header

--- frontend/src/components/layout/Layouts/PublicLayout.tsx
+++ frontend/src/components/layout/Layouts/PublicLayout.tsx
@@ -4,7 +4,7 @@ import PublicHeader from '../Header/PublicHeader';
 const PublicLayout: React.FC<{ children: React.ReactNode }> = ({children}) => {
     return (
         <div className="min-h-screen flex flex-col bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100">
-            <div className="shadow-lg rounded-b-3xl overflow-hidden">
+        <div className="shadow-lg overflow-hidden">
                 <PublicHeader/>
             </div>
             <main className="flex-1 w-full">
